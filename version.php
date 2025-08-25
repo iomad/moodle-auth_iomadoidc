f84af09dd8571c91b420f7adfdc297318abb0a5b
Derick Turner <derick@e-learndesign.co.uk>
2025-08-25 22:08:36 +0100

    IOMAD: need to set version minor numbers so we can have separate plugins in the plugin DB - 4.0.5

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release  = '4.5.6 (Build: 20250811)'; // Human-friendly version name
-$plugin->version = 2023100920;
+$plugin->version = 2023100920.405;
 $plugin->requires = 2023100900;
 $plugin->component = 'auth_iomadoidc';
 $plugin->dependencies = ['local_iomad' => 2024090401];
